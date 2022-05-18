9270a592f972e073472c88017376f607ff028027
maggick <maggick@users.noreply.github.com>
2022-05-18 16:27:00 +0200

    Intergrate with taskWarrior

--- timew.py
+++ timew.py
@@ -4,14 +4,37 @@ import os
 import pathlib
 import easygui
 import subprocess
+from tasklib import TaskWarrior
 
 def start():
-    myvar = easygui.enterbox("What task will you be working on? ", 'task')
-    if (myvar != None):
-        os.system('timew start ' + myvar +' > /dev/null')
+    w = TaskWarrior()
+    tasks = w.tasks.pending()
+    tasks_r = []
+    for task in tasks:
+        tasks_r.append(str(task['id']) + ": " +task['project'] + ": " +task['description'])
+
+    msg="What task will you be working on?"
+    title="task"
+    myvar = None
+    if len(tasks_r) !=0:
+      if len(tasks_r) ==1: # easygui choicebox want at least two options
+        tasks_r.append("")
+      myvar = easygui.choicebox(msg, title, tasks_r)
+
+    if (myvar != None and myvar != "Add more choices"):
+        os.system("task start " + str(myvar).split(':')[0] +"")
+
+    if (myvar == None or myvar == "Add more choices"):
+        myvar = easygui.enterbox(msg, title)
+        if (myvar != None):
+            os.system('timew start ' + myvar +' > /dev/null')
 
 def stop():
-    os.system('timew stop > /dev/null')
+    w = TaskWarrior()
+    tasks = w.tasks.pending()
+    for task in tasks:
+        os.system('task stop ' + str(task['id']))
+    os.system('timew stop')
 
 def left_click():
     start()
